56326ced7c8c4ae7857287836a39bd17d3144b15
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2022-10-23 12:42:49 +0530

    merge pork into express app

--- apps/pork/app.ts
+++ apps/pork/app.ts
@@ -1,11 +1,7 @@
-// this shim is required
-import { createExpressServer } from "routing-controllers";
+import { useExpressServer } from "routing-controllers";
 import { PorkController } from "./porkController";
 
-// creates express app, registers all controller routes and returns you express app instance
-const app = createExpressServer({
-  controllers: [PorkController], // we specify controllers we want to use
-});
-
-// run express application on port 3000
-app.listen(3000);
+export const porkApi = (app: any) =>
+  useExpressServer(app, {
+    controllers: [PorkController],
+  });
--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
+import { porkApi } from "../pork/app";
 
-const app = express();
+export const app = express();
 
 app.get("/info", (req, res) => {
   res.json({
     info: "Welcome to the API",
   });
 });
+
+porkApi(app);
 
 app.listen(3000, () => {
   console.log("Server has started");
